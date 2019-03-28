517cc7a22dab6200f4ba6421375bf6aa851a4694
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-03-28 23:02:23 +0000

    Funções onClick, setErrors, Internacionalização.

--- app/src/main/java/com/example/mygamelist/NovoJogo.java
+++ app/src/main/java/com/example/mygamelist/NovoJogo.java
@@ -1,5 +1,6 @@
 package com.example.mygamelist;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NovoJogo extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class NovoJogo extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,54 @@ public class NovoJogo extends AppCompatActivity {
         setContentView(R.layout.activity_novo_jogo);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner spinner = findViewById(R.id.spinnerGeneros);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.generos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = findViewById(R.id.spinnerPlataformas);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.plataformas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
     }
 
+    public void ConfirmarJogo(View view){
+        TextView textviewgenero = findViewById(R.id.textViewGeneros);
+        TextView textviewplataforma = findViewById(R.id.textViewPlataformas);
+        EditText editTextNomeJogo = findViewById(R.id.textViewNomeJogo);
+        Spinner spinnerGenero = findViewById(R.id.spinnerGeneros);
+        Spinner spinnerPlataforma = findViewById(R.id.spinnerPlataformas);
+
+        String NomeJogo = editTextNomeJogo.getText().toString();
+        int Genero = spinnerGenero.getSelectedItemPosition();
+        int Plataforma = spinnerPlataforma.getSelectedItemPosition();
+
+            if (NomeJogo.trim().length() == 0) {
+                editTextNomeJogo.setError(getString(R.string.nome_obrigatorio));
+            }else if (Genero == 0) {
+                textviewgenero.setError(getString(R.string.genero_obrigatorio));
+            }else if (Plataforma == 0) {
+                textviewplataforma.setError(getString(R.string.plataforma_obrigatorio));
+            }else {
+                finish();
+                Toast.makeText(this, getString(R.string.dados_sucesso), Toast.LENGTH_LONG).show();
+            }
+    }
+
+    public void CancelarJogo(View view){
+        finish();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
